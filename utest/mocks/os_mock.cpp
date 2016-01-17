5c16096299889b0195edb2f84f671289f20efe43
Marek Macko <macko.marek93@gmail.com>
2016-01-17 15:34:22 +0100

    Fixes to typo errors

--- utest/mocks/os_mock.cpp
+++ utest/mocks/os_mock.cpp
@@ -1,12 +1,12 @@
 #include "os_mock.h"
 
-IMPLEMENT_FUCNTION_MOCK3(os_socket_mock, os_socket, int(int, int, int));
-IMPLEMENT_FUCNTION_MOCK3(os_bind_mock, os_bind, int(int, const struct sockaddr *, socklen_t));
-IMPLEMENT_FUCNTION_MOCK2(os_listen_mock, os_listen, int(int, int));
-IMPLEMENT_FUCNTION_MOCK3(os_accept_mock, os_accept, int(int, struct sockaddr *, socklen_t *));
-IMPLEMENT_FUCNTION_MOCK3(os_read_mock, os_read, ssize_t(int, void *, size_t));
-IMPLEMENT_FUCNTION_MOCK3(os_write_mock, os_write, ssize_t(int, const void *, size_t));
-IMPLEMENT_FUCNTION_MOCK1(os_close_mock, os_close, int(int));
+IMPLEMENT_FUNCTION_MOCK3(os_socket_mock, os_socket, int(int, int, int));
+IMPLEMENT_FUNCTION_MOCK3(os_bind_mock, os_bind, int(int, const struct sockaddr *, socklen_t));
+IMPLEMENT_FUNCTION_MOCK2(os_listen_mock, os_listen, int(int, int));
+IMPLEMENT_FUNCTION_MOCK3(os_accept_mock, os_accept, int(int, struct sockaddr *, socklen_t *));
+IMPLEMENT_FUNCTION_MOCK3(os_read_mock, os_read, ssize_t(int, void *, size_t));
+IMPLEMENT_FUNCTION_MOCK3(os_write_mock, os_write, ssize_t(int, const void *, size_t));
+IMPLEMENT_FUNCTION_MOCK1(os_close_mock, os_close, int(int));
 
 IMPLEMENT_FUNCTION_MOCK4(os_epoll_ctl_mock, os_epol_ctl, int(int, int, int, struct epoll_event *));
 IMPLEMENT_FUNCTION_MOCK4(os_epoll_wait_mock, os_wait_ctl, int(int, struct epoll_event *, int, int));
--- utest/server_test.cpp
+++ utest/server_test.cpp
@@ -1,4 +1,5 @@
 extern "C" {
+	#include "conf_reader.h"
 	#include "acceptor_eh.h"
 	#include "client_eh.h"
 	#include "reactor.h"
@@ -6,29 +7,44 @@ extern "C" {
 
 #include "mocks/os_mock.h"
 
-#include "<gtest/gtest.h"
+#include <gtest/gtest.h>
 
 using namespace ::testing;
 
-TEST(server_test,  
+TEST(server_test, epoll_wait_fails_first_time)
+{
+	int srv_fd = 1;
+	int epoll_fd = 2;
+	
+	serv_sett ss;
+	ss.port = 3000;
+	ss.max_clients = 10;
 
-reactor *r = 0;
-event_handler* serv_eh = 0;
+	reactor *r = 0;
+	event_handler* serv_eh = 0;
 
-r = create_reactor(ss.max_clients);
-if (r == 0) {
-	return -1;
-}
+	r = create_reactor(ss.max_clients);
+	if (r == 0) {
+		return;
+	}
 
-serv_eh = construct_acceptor(r, &ss);
-if (serv_eh == 0) {
-	destroy_reactor(r);
-	return -1;
-}
+	os_epoll_ctl_mock ecn;
+	os_epoll_wait_mock ewm;
 
-signal(SIGPIPE, SIG_IGN);
-r->add_eh(r, serv_eh);
-r->event_loop(r);
-free(r);
-free(serv_eh);
+	EXPECT_FUNCTION_CALL(ecn, (epoll_fd, _, srv_fd, _)).WillOnce(Return(0));
+	EXPECT_FUNCTION_CALL(ewm, (epoll_fd, _, _, -1)).WillOnce(Return(-1));
+
+	serv_eh = construct_acceptor(r, &ss);
+	if (serv_eh == 0) {
+		destroy_reactor(r);
+		return;
+	}
+
+	signal(SIGPIPE, SIG_IGN);
+	r->add_eh(r, serv_eh);
+	r->event_loop(r);
+	free(r);
+	free(serv_eh);
+
+	ASSERT_TRUE(true);
 }
